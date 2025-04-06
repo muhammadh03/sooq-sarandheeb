d44b50455dd1b2b0d1c2c4726173fe63a3a43331
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2025-04-06 07:10:56 +0400

    new update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,38 +3,46 @@ import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 const Navbar = () => {
-  return (
-    <nav className='bg-[#3FC602] text-white flex items-center justify-between px-8 py-6 shadow-xl shadow-gray-300'>
-      <div className='flex items-center gap-2 py-2 px-6 hover:bg-red-600 rounded-lg'>
-        <GridViewRoundedIcon />
-        <p>Category</p>
-      </div>
-      
-      <div>
-        <ul className='flex items-center gap-6 sm:hidden'>
-            <li>
-                <a href="#" className='nav-link'>Baby Products</a>
-            </li>
-            <li>
-                <a href="#" className='nav-link'>Fresh Food</a>
-            </li>
-            <li>
-                <a href="#" className='nav-link'>Dairy</a>
-            </li>
-            <li>
-                <a href="#" className='nav-link'>Beverages</a>
-            </li>
-        </ul>
-      </div>
+    return (
+        <nav className='bg-[#3FC602] text-white shadow-xl shadow-gray-300'>
+            <div className='container flex items-center justify-between'>
+                <div className='flex items-center gap-2 py-2 px-3 hover:bg-red-600 rounded-lg'>
+                    <GridViewRoundedIcon  />
+                    <p>Category</p>
+                </div>
 
-      <div className='flex items-center '>
-        <input type="text" placeholder="Search..." className='w-72 border-2 px-4 py-2 rounded-full relative' />
-        <div className='bg-red-600 p-1.5 rounded-full absolute right-9'>
-            <MagnifyingGlassIcon strokeWidth={2} className=' size-6 cursor-pointer' />
-        </div>
-      </div>
-    </nav>
-  )
+                <div className='hidden lg:block'>
+                    <ul className='flex items-center gap-6'>
+                        <li>
+                            <a href="#" className='nav-link'>Baby Products</a>
+                        </li>
+                        <li>
+                            <a href="#" className='nav-link'>Fresh Food</a>
+                        </li>
+                        <li>
+                            <a href="#" className='nav-link'>Dairy</a>
+                        </li>
+                        <li>
+                            <a href="#" className='nav-link'>Beverages</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className='flex items-center'>
+                    <input type="text" placeholder="Search..." className='w-72 lg:w-80 border-2 px-4 py-2 rounded-full relative' />
+                    <MagnifyingGlassIcon strokeWidth={2} className='bg-red-600 p-1.5 rounded-full absolute  size-9 right-5 md:right-9 lg:right-9.5 cursor-pointer' />
+                    {/* <div className=''>
+                        <MagnifyingGlassIcon strokeWidth={2} className='bg-red-600 p-1.5 rounded-full absolute right-7.5 lg:right-22 size-6 cursor-pointer' />
+                    </div> */}
+                </div>
+
+                {/* HAMBURGER */}
+                {/* <div>
+
+                </div> */}
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
