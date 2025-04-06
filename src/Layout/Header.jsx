06b9330019da42a78afa7e2c700ce8cdfd987039
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2025-04-06 07:16:07 +0400

    Navbar update

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import TopBar from '../Components/TopBar';
-import Navbar from '../Components/Navbar';
+import NavBar from '../Components/NavBar';
 
 const Header = () => {
   return (
     <div>
       <TopBar />
-      <Navbar />
+      <NavBar />
     </div>
   )
 }
