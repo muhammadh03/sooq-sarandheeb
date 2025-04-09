6a7982c404025f0c858bac469f084ec0789727d5
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2025-04-09 07:40:57 +0400

    card component update

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,13 @@
 import CardFour from '../assets/cardFour.jpg'
 
-const Card = () => {
+const Card = ({title, image}) => {
   return (
     <div className='my-2'>
       <div className='xs:w-[6.5rem] md:w-[7.6rem] lg:w-48 rounded-b-xl shadow-lg shadow-gray-400'>
-        <img src={CardFour} alt="" className='rounded-t-xl' />
-        <div className='text-center p-2'>
+        {image}
+        <div className='text-center h-18 lg:h-14 p-2'>
             <p>
-                fresh sea food
+                {title}
             </p>
         </div>
       </div>
--- src/Layout/HomeContent.jsx
+++ src/Layout/HomeContent.jsx
@@ -2,21 +2,68 @@ import React from 'react'
 import Banner from '../Components/Banner';
 import Card from '../Components/Card';
 
+import CardOne from '../assets/cardOne.jpg';
+import CardTwo from '../assets/cardTwo.jpg';
+import CardThree from '../assets/cardThree.jpg';
+import CardFour from '../assets/cardFour.jpg';
+import CardFive from '../assets/cardFive.jpg';
+
+
+const CardDetails = [
+    {
+        title : 'Premium Perfumes',
+        image : <img src={CardOne} alt="" className='rounded-t-xl' />
+    },
+    {
+        title : 'Frozen Products',
+        image : <img src={CardTwo} alt="" className='rounded-t-xl' />
+    },
+    {
+        title : 'Chilled Products',
+        image : <img src={CardThree} alt="" className='rounded-t-xl' />
+    },
+    {
+        title : 'Fresh Sea Food',
+        image : <img src={CardFour} alt="" className='rounded-t-xl' />
+    },
+    {
+        title : 'Beverages',
+        image : <img src={CardFive} alt="" className='rounded-t-xl' />
+    },
+    {
+        title : 'Premium Perfumes',
+        image : <img src={CardOne} alt="" className='rounded-t-xl' />
+    },
+    {
+        title : 'Frozen Products',
+        image : <img src={CardTwo} alt="" className='rounded-t-xl' />
+    },
+    {
+        title : 'Chilled Products',
+        image : <img src={CardThree} alt="" className='rounded-t-xl' />
+    },
+    {
+        title : 'Fresh Sea Food',
+        image : <img src={CardFour} alt="" className='rounded-t-xl' />
+    },
+    {
+        title : 'Beverages',
+        image : <img src={CardFive} alt="" className='rounded-t-xl' />
+    }
+]
+
 const HomeContent = () => {
   return (
     <div>
       <Banner />
       <div className='container mt-4 grid grid-cols-3 md:grid-cols-5 gap-4 md:gap-6 lg:gap-12'>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {CardDetails.map(({title, image}) => (
+        <Card 
+        key={title}
+        title={title}
+        image={image}
+        />
+      ))}
       </div>
     </div>
   )
