e0c3ad612356ff8b2c078cc98f33db2cf5a7c54f
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2025-04-10 07:38:03 +0400

    card update

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,33 @@
-import CardFour from '../assets/cardFour.jpg'
+import { useEffect, useState } from 'react';
+// import CardFour from '../assets/cardFour.jpg';
 
-const Card = ({title, image}) => {
+const Card = ({title, itemImage, offerImage}) => {
+
+  const [showOffer, setShowOffer] = useState(false); // State to toggle between item image and offer image
+
+  useEffect(() => {
+    if (offerImage) {
+      const interval = setInterval(() => {
+        setShowOffer((prevShowOffer) => !prevShowOffer); // Toggle the state every 2 seconds
+      }, 2000);
+
+      return () => clearInterval(interval); // Cleanup the interval on component unmount
+    }
+  }, [offerImage]);
+
   return (
     <div className='my-2'>
-      <div className='xs:w-[6.5rem] md:w-[7.6rem] lg:w-48 rounded-b-xl shadow-lg shadow-gray-400'>
-        {image}
-        <div className='text-center h-18 lg:h-14 p-2'>
+      <div className='xs:w-[6.5rem] md:w-[7.6rem] lg:w-48 rounded-b-xl shadow-md shadow-gray-400 hover:shadow-xl cursor-pointer duration-300'>
+        {/* <img src={itemImage} alt="" className='rounded-t-xl' /> */}
+        {/* Conditional rendering of images */}
+        <div className="relative w-full h-full overflow-hidden rounded-t-xl">
+          {offerImage && showOffer ? (
+            <img src={offerImage} alt="Offer" className="w-full h-full object-cover" />
+          ) : (
+            <img src={itemImage} alt={title} className="w-full h-full object-cover" />
+          )}
+        </div>
+        <div className='text-center h-18 lg:h-14 p-2 rounded-b-xl'>
             <p>
                 {title}
             </p>
--- src/Layout/HomeContent.jsx
+++ src/Layout/HomeContent.jsx
@@ -7,66 +7,79 @@ import CardTwo from '../assets/cardTwo.jpg';
 import CardThree from '../assets/cardThree.jpg';
 import CardFour from '../assets/cardFour.jpg';
 import CardFive from '../assets/cardFive.jpg';
+import CardSeven from '../assets/cardSeven.jpg';
 
+import OfferImage from '../assets/offerImage.jpg';
 
 const CardDetails = [
     {
-        title : 'Premium Perfumes',
-        image : <img src={CardOne} alt="" className='rounded-t-xl' />
+      title: 'Premium Perfumes',
+      itemImage: CardOne,
     },
     {
-        title : 'Frozen Products',
-        image : <img src={CardTwo} alt="" className='rounded-t-xl' />
+      title: 'Frozen Products',
+      itemImage: CardTwo,
     },
     {
-        title : 'Chilled Products',
-        image : <img src={CardThree} alt="" className='rounded-t-xl' />
+      title: 'Chilled Products',
+      itemImage: CardThree,
     },
     {
-        title : 'Fresh Sea Food',
-        image : <img src={CardFour} alt="" className='rounded-t-xl' />
+      title: 'Fresh Sea Food',
+      itemImage: CardFour,
     },
     {
-        title : 'Beverages',
-        image : <img src={CardFive} alt="" className='rounded-t-xl' />
+      title: 'Beverages',
+      itemImage: CardFive,
     },
     {
-        title : 'Premium Perfumes',
-        image : <img src={CardOne} alt="" className='rounded-t-xl' />
-    },
-    {
-        title : 'Frozen Products',
-        image : <img src={CardTwo} alt="" className='rounded-t-xl' />
-    },
-    {
-        title : 'Chilled Products',
-        image : <img src={CardThree} alt="" className='rounded-t-xl' />
-    },
-    {
-        title : 'Fresh Sea Food',
-        image : <img src={CardFour} alt="" className='rounded-t-xl' />
-    },
-    {
-        title : 'Beverages',
-        image : <img src={CardFive} alt="" className='rounded-t-xl' />
-    }
-]
+        title: 'Premium Perfumes',
+        itemImage: CardOne,
+      },
+      {
+        title: 'Home Furniture',
+        itemImage: CardSeven,
+      },
+      {
+        title: 'Chilled Products',
+        itemImage: CardThree,
+      },
+      {
+        title: 'Fresh Sea Food',
+        itemImage: CardFour,
+      },
+      {
+        title: 'Beverages',
+        itemImage: CardFive,
+      },
+  ];
 
 const HomeContent = () => {
-  return (
-    <div>
-      <Banner />
-      <div className='container mt-4 grid grid-cols-3 md:grid-cols-5 gap-4 md:gap-6 lg:gap-12'>
-      {CardDetails.map(({title, image}) => (
-        <Card 
-        key={title}
-        title={title}
-        image={image}
-        />
-      ))}
-      </div>
-    </div>
-  )
+
+    const offerMapping = {
+        'Premium Perfumes': OfferImage,
+        'Home Furniture': OfferImage,
+        'Beverages': OfferImage,
+    };
+      
+    return (
+        <div>
+            <Banner />
+            <div className='container mt-4 grid grid-cols-3 md:grid-cols-5 gap-4 md:gap-6 lg:gap-12'>
+                {CardDetails.map(({ title, itemImage }) => {
+                    const offerImage = offerMapping[title] || null; // Get offer image from mapping, or null if not defined
+                    return (
+                        <Card
+                        key={title}
+                        title={title}
+                        itemImage={itemImage}
+                        offerImage={offerImage}
+                    />
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default HomeContent;
