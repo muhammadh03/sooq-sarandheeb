e630af152ef05bf0f423941630a9d6f5f5ae5ecb
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2025-04-16 07:48:11 +0400

    again push

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import TopBar from '../Components/TopBar';
 import NavBar from '../Components/NavBar';
-import SecondNavbar from '../Components/SecondNavbar';
+// import SecondNavbar from '../Components/SecondNavbar';
 
 const Header = () => {
   return (
